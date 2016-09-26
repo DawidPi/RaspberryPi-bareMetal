f1d02f71f406cf21e4e8815541df6ed1d0f98dac
Dawid Pilarski <pilarski.dawid94@gmail.com>
2016-09-26 19:59:32 +0200

    Added support for gtests in CMake files

---
+++ PiManager/src/Tests/test_main.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Dawid on 26.09.2016.
+//
+
--- PiManager/src/options.cmake
+++ PiManager/src/options.cmake
@@ -1,7 +1,16 @@
-set(QT5_PACKAGE_DIR "" CACHE FILEPATH "path to the place, where package to the QT5_Dir exists, so that cmake can find package")
+set(QT5_PACKAGE_DIR "" CACHE FILEPATH "path to the place, where package to the QT5_Dir exists")
 
 if (QT5_PACKAGE_DIR STREQUAL "")
     message(FATAL_ERROR "QT5_PACKAGE_DIR empty: Please specify QT5_PACKAGE_DIR path")
 else ()
     file(GLOB QT_DLLS QT5_PACKAGE_DIR "*.dll")
+endif ()
+
+
+set(GTEST_DIR "" CACHE FILEPATH "path to the downloaded gtest library")
+if (GTEST_DIR STREQUAL "")
+    message(STATUS "GTEST_DIR empty: Configuring project without Tests")
+    set(USE_TESTS BOOL OFF)
+else ()
+    set(USE_TESTS BOOL ON)
 endif ()
